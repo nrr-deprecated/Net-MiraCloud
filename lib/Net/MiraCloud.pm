70e05412cf01c1188f090c224cffad0f76bd5613
Nathaniel R Reindl <nrr@corvidae.org>
2012-05-04 16:41:06 -0500

    Walk [N]orth to interact with the Plaid-Tongued Devils.

---
+++ lib/Net/MiraCloud.pm
@@ -0,0 +1,22 @@
+use strictures;
+package Net::MiraCloud;
+
+use 5.010;
+use utf8;
+use open qw(:std :utf8);
+use charnames qw(:full :short);
+
+use Carp;
+
+# VERSION
+# ABSTRACT:
+# ENCODING: utf-8
+
+=head1 SYNOPSIS
+
+A library for interacting with the API for Contegix's MiraCloud
+virtual private server orchestrator.
+
+=cut
+
+1;
